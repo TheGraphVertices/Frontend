7c5b85faf46c0c0ff8bd7ca28259908b2cf2fa8f
DripSwag <mimirani33@gmail.com>
2023-03-07 15:50:27 +0000

    Temperature bar now moves depending on temperautre

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -1,11 +1,20 @@
 function Temperature(){
+  const portion = Math.round((10 / 30) * 100)
+
+  const portionStyle = {
+    clipPath: `polygon(0% 0%, 0% 50%, 50% 50%, ${portion}% 0%)`,
+  }
+
   return(
-	<div className="bg-white rounded-full p-4 w-[65%] flex justify-center items-center flex-column text-center shadow-[0px_10px_50px_-12px_rgb(0,0,0,0.3)] aspect-square">
-      <div>
-        <h1 className="text-7xl font-extrabold">12°C</h1>
-        <h2 className="text-2xl font-extrabold">Temperature</h2>
-      </div>
-    </div>	
+    <div className="relative w-[65%] aspect-square">
+      <div className="bg-white w-full h-full rounded-full flex justify-center items-center flex-column text-center shadow-[0px_10px_50px_-12px_rgb(0,0,0,0.3)] absolute z-20">
+        <div>
+          <h1 className="text-7xl font-extrabold">12°C</h1>
+          <h2 className="text-2xl font-extrabold">Temperature</h2>
+        </div>
+      </div>	
+      <div className="w-[110%] h-[110%] rounded-full absolute z-10 -translate-x-[5%] -translate-y-[5%] bg-orange-500" style={portionStyle}></div>
+    </div>
   )
 }
 export default Temperature
