f0b70a661ac063b6cd1c9b4c038cc1a82dad679c
DripSwag <mimirani33@gmail.com>
2023-03-19 15:31:51 +0000

    Removed some testing elements and fixed some incorrect code

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Layout from './pages/Layout.jsx';
 import UserHomePage from './pages/UserHomePage.jsx'
 import Login from './pages/Login';
 import CreateAccount from './pages/CreateAccount';
-import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
           <Route index element={<Login />} />
           <Route path="userhomepage" element={<UserHomePage />} />
           <Route path="register" element={<CreateAccount />} />
-          <Route path='forgotpassword' element={<ForgotPassword />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,17 +9,9 @@ function Login(){
   const [password, setPassword] = useState()
   const [wrongPassword, setWrongPassword] = useState(false)
 
-  const redirectTemp = () => {
-    navigate("/userhomepage", {
-      state: {
-        uid: "cd38c98d-8d6b-4373-ab87-e34618d9ee95"
-      }
-    })
-  }
-
   const loginRedirect = async () => {
     const response = await fetch(`https://api.jeremypetch.com/user/?fname=${firstName}&lname=${lastName}&address=${address}&password=${password}`)
-    if(response.status == 401){
+    if(response.status === 401){
       setWrongPassword(true)
     }
     else{
@@ -35,7 +27,7 @@ function Login(){
   return(
   <section className="flex items-center flex-col justify-center h-screen gap-8">
     <h1 className="font-semibold text-2xl">Welcome Back</h1>
-    <p className={`text-lg ${wrongPassword ? "hidden" : "block"}`}>Password is incorrect</p>
+    <p className={wrongPassword ? "block text-lg" : "hidden"}>Password is incorrect</p>
     <div className="max-w-[400px] w-full px-8">
       <form className="my-4">
         <label className="flex flex-col border-b-4 gap-2">
@@ -61,13 +53,11 @@ function Login(){
           <input onChange={(data) => {setPassword(data.target.value)}} type="password" className="outline-0 py-2 text-xl font-semibold"/>
         </label>
       </form>
-      <a href="/forgotpassword" className="text-slate-600 hover:underline">Forgot Password?</a>
     </div>
     <div className="flex flex-row justify-between max-w-[400px] px-8 w-full">
       <button onClick={loginRedirect} className="bg-black rounded-lg text-white px-6 py-2">Sign In</button>
       <a href="/register" className="border-2 rounded-lg border-slate-300 px-4 py-2">Create Account</a>
     </div>
-    <button onClick={redirectTemp}>Test</button>
   </section>
   )
 }
