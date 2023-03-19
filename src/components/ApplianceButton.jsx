f62e2116a03dc4be576239c87a81de8881936cf2
DripSwag <mimirani33@gmail.com>
2023-03-19 15:48:48 +0000

    Fixed appliance button bad code

--- src/components/ApplianceButton.jsx
+++ src/components/ApplianceButton.jsx
@@ -1,5 +1,3 @@
-import {useState} from "react"
-
 function ApplianceButton({ name, putFunction, on }){
   return(
 	<div onClick={() => {putFunction()}} className={`${on ? "bg-gradient-to-b from-[#FF3E3E] to-[#FFA337] text-white" : "bg-[#D9D9D9]"} w-full h-16 flex justify-center items-center rounded-lg`}>
